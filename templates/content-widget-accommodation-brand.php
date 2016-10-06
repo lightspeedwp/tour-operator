5349cad63aa992da973602a880cdfd0de2f8be9f
Fernando Tessmann <nandotess85@gmail.com>
2016-10-06 15:48:42 -0300

    code sniffer fixes

--- templates/content-widget-accommodation-brand.php
+++ templates/content-widget-accommodation-brand.php
@@ -10,22 +10,22 @@ global $term,$taxonomy,$disable_placeholder;
 $url_taxonomy = 'brand';
 ?>
 
-<article id="term-<?php echo $term->term_id; ?>" class="term-<?php echo $term->term_id; ?> <?php echo $taxonomy; ?> type-<?php echo $taxonomy; ?> status-publish hentry">
+<article id="term-<?php echo esc_attr( $term->term_id ); ?>" class="term-<?php echo esc_attr( $term->term_id ); ?> <?php echo esc_attr( $taxonomy ); ?> type-<?php echo esc_attr( $taxonomy ); ?> status-publish hentry">
  	<?php if('1' !== $disable_placeholder && true !== $disable_placeholder) { ?>
 		<?php if ( lsx_has_term_thumbnail($term->term_id) ) : ?>
 			<div class="thumbnail">
-				<a href="<?php echo home_url($taxonomy.'/'.$term->slug.'/'); ?>">
+				<a href="<?php echo esc_url( home_url($taxonomy.'/'.$term->slug.'/') ); ?>">
 					<?php lsx_term_thumbnail( $term->term_id,'lsx-thumbnail-wide' ); ?>
 				</a>
 			</div>	
 		<?php else: ?>	
 			<div class="thumbnail">
-				<a href="<?php echo home_url($taxonomy.'/'.$term->slug.'/'); ?>">
-					<img alt="Placeholder" class="attachment-responsive wp-post-image lsx-responsive" src="<?php echo LSX_Placeholders::placeholder_url() . ( parse_url( LSX_Placeholders::placeholder_url(), PHP_URL_QUERY ) ? '&' : '?' ); ?>w=350&amp;h=230">
+				<a href="<?php echo esc_url( home_url($taxonomy.'/'.$term->slug.'/') ); ?>">
+					<img alt="Placeholder" class="attachment-responsive wp-post-image lsx-responsive" src="<?php echo esc_attr( LSX_Placeholders::placeholder_url() . ( parse_url( LSX_Placeholders::placeholder_url(), PHP_URL_QUERY ) ? '&' : '?' ) ); ?>w=350&amp;h=230">
 				</a>	
 			</div>				
 		<?php endif; ?>
 	<?php } ?>
 	
-	<h4 class="title"><a href="<?php echo home_url($url_taxonomy.'/'.$term->slug.'/'); ?>"><?php echo apply_filters('the_title', $term->name); ?></a></h4>
+	<h4 class="title"><a href="<?php echo esc_url( home_url($url_taxonomy.'/'.$term->slug.'/') ); ?>"><?php echo esc_html( apply_filters('the_title', $term->name) ); ?></a></h4>
 </article>
