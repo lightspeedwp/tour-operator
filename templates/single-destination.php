ca58106a538d04ea69d05f5ca6399c2021568e69
Fernando Tessmann <nandotess85@gmail.com>
2016-10-06 16:03:00 -0300

    code sniffer fixes

--- templates/single-destination.php
+++ templates/single-destination.php
@@ -8,7 +8,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area <?php echo esc_attr( lsx_main_class() ); ?>">">
+	<div id="primary" class="content-area <?php echo esc_attr( lsx_main_class() ); ?>">
 
 		<?php lsx_content_before(); ?>
 		
