4476092c33b0886bccc0ed1257764e6e2948a03b
Warwick Booth <warwick@lsdev.biz>
2016-10-20 06:21:43 +0200

    Renaming the API tab page

--- includes/settings/api.php
+++ includes/settings/api.php
@@ -2,8 +2,7 @@
 
 	<ul class="ui-tab-nav">
 		<li><a href="#ui-settings" class="active"><?php esc_html_e('Settings','tour-operator'); ?></a></li>
-		<li><a href="#ui-keys"><?php esc_html_e('Keys','tour-operator'); ?></a></li>
-		<li><a href="#ui-webhooks"><?php esc_html_e('Webhooks','tour-operator'); ?></a></li>
+		<li><a href="#ui-keys"><?php esc_html_e('License Keys','tour-operator'); ?></a></li>
 	</ul>
 
 	<div id="ui-settings" class="ui-tab active">
@@ -23,11 +22,6 @@
 			</tbody>
 		</table>
 		<?php do_action('to_framework_api_tab_bottom','api'); ?>
-	</div>
-
-	<div id="ui-webhooks" class="ui-tab">
-	  <h3><?php esc_html_e('Webhooks','tour-operator'); ?></h3>
-	  <p>Enter your text here</p>
 	</div>
 
 </div>
