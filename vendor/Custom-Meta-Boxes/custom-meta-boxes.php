47049ef4b323ec2cf8e4e70ac57ff1eedd63698a
Warwick Booth <warwick@lsdev.biz>
2016-12-15 07:02:45 +0200

    Removing the plugin headers

--- vendor/Custom-Meta-Boxes/custom-meta-boxes.php
+++ vendor/Custom-Meta-Boxes/custom-meta-boxes.php
@@ -1,27 +1,4 @@
 <?php
-/*
-Plugin Name: Custom Meta Boxes
-Plugin URI: https://github.com/humanmade/Custom-Meta-Boxes
-Description: Lets you easily create metaboxes with custom fields that will blow your mind. Originally a fork of https://github.com/jaredatch/Custom-Metaboxes-and-Fields-for-WordPress.
-Version: 1.0.2
-License: GPL3
-Author: Human Made Limited
-Author URI: http://hmn.md
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License, version 2, as
-published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
 if ( ! defined( 'CMB_DEV') )
 	define( 'CMB_DEV', false );
 
