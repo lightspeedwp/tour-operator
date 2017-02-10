2822d2644f960eadcdbfb22ec3c5025f8e8e5eb8
Fernando Tessmann <nandotess85@gmail.com>
2017-02-10 09:28:28 -0200

    * Added TO Search as subtab on LSX TO settings page

--- includes/settings/tour.php
+++ includes/settings/tour.php
@@ -2,6 +2,9 @@
 
 	<ul class="ui-tab-nav">
 		<li><a href="#ui-general" class="active"><?php esc_html_e('General','tour-operator'); ?></a></li>
+		<?php if(class_exists('LSX_TO_Search')) { ?>
+			<li><a href="#ui-search"><?php esc_html_e('Search','tour-operator'); ?></a></li>
+		<?php } ?>
 		<li><a href="#ui-placeholders"><?php esc_html_e('Placeholders','tour-operator'); ?></a></li>
 		<li><a href="#ui-archives"><?php esc_html_e('Archives','tour-operator'); ?></a></li>
 		<li><a href="#ui-single"><?php esc_html_e('Single','tour-operator'); ?></a></li>
@@ -14,6 +17,16 @@
 			</tbody>
 		</table>
 	</div>
+
+	<?php if(class_exists('LSX_TO_Search')) { ?>
+		<div id="ui-search" class="ui-tab">
+			<table class="form-table">
+				<tbody>
+				<?php do_action('lsx_to_framework_tour_tab_content','tour','search'); ?>
+				</tbody>
+			</table>
+		</div>
+	<?php } ?>
 
 	<div id="ui-placeholders" class="ui-tab">
 		<table class="form-table">
