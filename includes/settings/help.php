01387dba91df4b1f2518dd73e603fbe97d4ae987
Fernando Tessmann <nandotess85@gmail.com>
2017-03-09 09:17:00 -0300

    Closes #19 (Incorrect Link)

--- includes/settings/help.php
+++ includes/settings/help.php
@@ -101,7 +101,7 @@
 					<div class="box news">
 						<h3><?php esc_html_e( 'Support', 'tour-operator' ); ?></h3>
 						<p><?php esc_html_e( 'If the documentation is not getting you where you need to be, you can contact us directly for support and assistance.', 'tour-operator' ); ?></p>
-						<p><?php esc_html_e( 'Please view our support policy', 'tour-operator' ); ?> <a href="<?php echo esc_url('/');?> " target="_blank"><?php esc_html_e( 'here.', 'tour-operator' ); ?></a></p>
+						<p><?php esc_html_e( 'Please view our support policy', 'tour-operator' ); ?> <a href="https://www.lsdev.biz/plugin-support-policy/" target="_blank"><?php esc_html_e( 'here.', 'tour-operator' ); ?></a></p>
 						<p><?php esc_html_e( 'You can contact us for support at', 'tour-operator' ); ?> <a href="mailto:<?php echo esc_url('support@lsdev.biz');?> "><?php esc_html_e( 'support@lsdev.biz.', 'tour-operator' ); ?></a></p>
 						<p><?php esc_html_e( 'Support from the LightSpeed Team is only provided to user who have purchased one of our', 'tour-operator' ); ?> <strong><?php esc_html_e( 'Premium Add-ons', 'tour-operator' ); ?></strong></p>
 					</div>
