282da91133b669b7b09c6b72bd6689fa35887594
Warwick Booth <warwick@lsdev.biz>
2017-06-06 17:27:38 +0200

    Fixing the General Tab display

--- includes/settings/display.php
+++ includes/settings/display.php
@@ -1,59 +1,64 @@
 <div class="uix-field-wrapper">
-
+    <?php $class = 'active'; ?>
 	<ul class="ui-tab-nav">
-		<li><a href="#ui-general" class="active"><?php esc_html_e('General','tour-operator'); ?></a></li>
+		<?php if(class_exists('LSX_Banners')) { ?>
+		    <li><a href="#ui-general" class="<?php echo wp_kses_post($class); ?>"><?php esc_html_e('General','tour-operator'); ?></a></li>
+		<?php $class = ''; } ?>
 		<?php if(class_exists('LSX_Currencies')) { ?>
-			<li><a href="#ui-currencies"><?php esc_html_e('Currency Switcher','tour-operator'); ?></a></li>
-		<?php } ?>
+			<li><a href="#ui-currencies" class="<?php echo wp_kses_post($class); ?>"><?php esc_html_e('Currency Switcher','tour-operator'); ?></a></li>
+		<?php $class = ''; } ?>
 		<?php if(class_exists('LSX_TO_Maps')) { ?>
-			<li><a href="#ui-maps"><?php esc_html_e('Maps','tour-operator'); ?></a></li>
-		<?php } ?>
+			<li><a href="#ui-maps" class="<?php echo wp_kses_post($class); ?>"><?php esc_html_e('Maps','tour-operator'); ?></a></li>
+		<?php $class = ''; } ?>
 		<?php if(class_exists('LSX_TO_Search')) { ?>
-			<li><a href="#ui-search"><?php esc_html_e('Search','tour-operator'); ?></a></li>
-		<?php } ?>
-		<li><a href="#ui-placeholders"><?php esc_html_e('Placeholders','tour-operator'); ?></a></li>
+			<li><a href="#ui-search" class="<?php echo wp_kses_post($class); ?>"><?php esc_html_e('Search','tour-operator'); ?></a></li>
+		<?php $class = ''; } ?>
+		<li><a href="#ui-placeholders" class="<?php echo wp_kses_post($class); ?>"><?php esc_html_e('Placeholders','tour-operator'); ?></a></li>
 		<li><a href="#ui-advanced"><?php esc_html_e('Advanced','tour-operator'); ?></a></li>
 	</ul>
 
-	<div id="ui-general" class="ui-tab active">
-		<table class="form-table">
-			<tbody>
-			<?php do_action('lsx_to_framework_display_tab_content','basic'); ?>
-			</tbody>
-		</table>	  
-	</div>
+	<?php $class = 'active'; ?>
+	<?php if(class_exists('LSX_Banners')) { ?>
+        <div id="ui-general" class="ui-tab <?php echo wp_kses_post($class); ?>">
+            <table class="form-table">
+                <tbody>
+                <?php do_action('lsx_to_framework_display_tab_content','basic'); ?>
+                </tbody>
+            </table>
+        </div>
+    <?php $class = ''; } ?>
 
 	<?php if(class_exists('LSX_Currencies')) { ?>
-		<div id="ui-currencies" class="ui-tab">
+		<div id="ui-currencies" class="ui-tab <?php echo wp_kses_post($class); ?>">
 			<table class="form-table">
 				<tbody>
 				<?php do_action('lsx_to_framework_display_tab_content','currency_switcher'); ?>
 				</tbody>
 			</table>
 		</div>
-	<?php } ?>
+	<?php $class = ''; } ?>
 
 	<?php if(class_exists('LSX_TO_Maps')) { ?>
-		<div id="ui-maps" class="ui-tab">
+		<div id="ui-maps" class="ui-tab <?php echo wp_kses_post($class); ?>">
 			<table class="form-table">
 				<tbody>
 				<?php do_action('lsx_to_framework_display_tab_content','maps'); ?>
 				</tbody>
 			</table>
 		</div>
-	<?php } ?>
+	<?php $class = ''; } ?>
 
 	<?php if(class_exists('LSX_TO_Search')) { ?>
-		<div id="ui-search" class="ui-tab">
+		<div id="ui-search" class="ui-tab <?php echo wp_kses_post($class); ?>">
 			<table class="form-table">
 				<tbody>
 				<?php do_action('lsx_to_framework_display_tab_content','search'); ?>
 				</tbody>
 			</table>
 		</div>
-	<?php } ?>
+	<?php $class = ''; } ?>
 
-	<div id="ui-placeholders" class="ui-tab">
+	<div id="ui-placeholders" class="ui-tab <?php echo wp_kses_post($class); ?>">
 		<table class="form-table">
 			<tbody>
 			<?php do_action('lsx_to_framework_display_tab_content','placeholders'); ?>
