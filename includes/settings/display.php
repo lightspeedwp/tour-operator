749ee4d3b0ef3c5163a177bda139078b5e637d55
Fernando Tessmann <nandotess85@gmail.com>
2017-06-12 16:07:49 -0300

    * Fix - LSX tabs working integrated with TO tabs (dashboard settings)

--- includes/settings/display.php
+++ includes/settings/display.php
@@ -1,67 +1,144 @@
 <div class="uix-field-wrapper">
     <?php $class = 'active'; ?>
+
 	<ul class="ui-tab-nav">
-		<?php if(class_exists('LSX_Banners')) { ?>
-		    <li><a href="#ui-general" class="<?php echo wp_kses_post($class); ?>"><?php esc_html_e('General','tour-operator'); ?></a></li>
+		<?php if ( class_exists( 'LSX_Banners' ) ) { ?>
+			<li><a href="#ui-placeholders" class="active"><?php esc_html_e( 'Placeholders', 'tour-operator' ); ?></a></li>
 		<?php $class = ''; } ?>
-		<?php if(class_exists('LSX_Currencies')) { ?>
-			<li><a href="#ui-currencies" class="<?php echo wp_kses_post($class); ?>"><?php esc_html_e('Currency Switcher','tour-operator'); ?></a></li>
+
+		<?php if ( class_exists( 'LSX_Currencies' ) ) { ?>
+			<li><a href="#ui-currencies" class="<?php echo esc_attr( $class ) ?>"><?php esc_html_e( 'Currencies', 'tour-operator' ); ?></a></li>
 		<?php $class = ''; } ?>
-		<?php if(class_exists('LSX_TO_Maps')) { ?>
-			<li><a href="#ui-maps" class="<?php echo wp_kses_post($class); ?>"><?php esc_html_e('Maps','tour-operator'); ?></a></li>
+
+		<?php if ( class_exists( 'LSX_Team' ) ) { ?>
+			<li><a href="#ui-team" class="<?php echo esc_attr( $class ) ?>"><?php esc_html_e( 'Team', 'tour-operator' ); ?></a></li>
 		<?php $class = ''; } ?>
-		<?php if(class_exists('LSX_TO_Search')) { ?>
-			<li><a href="#ui-search" class="<?php echo wp_kses_post($class); ?>"><?php esc_html_e('Search','tour-operator'); ?></a></li>
+
+		<?php if ( class_exists( 'LSX_Testimonials' ) ) { ?>
+			<li><a href="#ui-testimonials" class="<?php echo esc_attr( $class ) ?>"><?php esc_html_e( 'Testimonials', 'tour-operator' ); ?></a></li>
 		<?php $class = ''; } ?>
-		<li><a href="#ui-placeholders" class="<?php echo wp_kses_post($class); ?>"><?php esc_html_e('Placeholders','tour-operator'); ?></a></li>
-		<li><a href="#ui-advanced"><?php esc_html_e('Advanced','tour-operator'); ?></a></li>
+
+		<?php if ( class_exists( 'LSX_Projects' ) ) { ?>
+			<li><a href="#ui-projects" class="<?php echo esc_attr( $class ) ?>"><?php esc_html_e( 'Projects', 'tour-operator' ); ?></a></li>
+		<?php $class = ''; } ?>
+
+		<?php if ( class_exists( 'LSX_Blog_Customizer' ) ) { ?>
+			<li><a href="#ui-blog-customizer" class="<?php echo esc_attr( $class ) ?>"><?php esc_html_e( 'Blog Customizer (posts widget)', 'tour-operator' ); ?></a></li>
+		<?php $class = ''; } ?>
+
+		<?php if ( class_exists( 'LSX_Sharing' ) ) { ?>
+			<li><a href="#ui-sharing" class="<?php echo esc_attr( $class ) ?>"><?php esc_html_e( 'Sharing', 'tour-operator' ); ?></a></li>
+		<?php $class = ''; } ?>
+
+		<?php if ( class_exists( 'LSX_TO_Maps' ) ) { ?>
+			<li><a href="#ui-maps" class="<?php echo esc_attr( $class ); ?>"><?php esc_html_e( 'Maps', 'tour-operator' ); ?></a></li>
+		<?php $class = ''; } ?>
+
+		<?php if ( class_exists( 'LSX_TO_Search' ) ) { ?>
+			<li><a href="#ui-search" class="<?php echo esc_attr( $class ); ?>"><?php esc_html_e( 'Search', 'tour-operator' ); ?></a></li>
+		<?php $class = ''; } ?>
+
+		<li><a href="#ui-placeholders" class="<?php echo esc_attr( $class ); ?>"><?php esc_html_e( 'Placeholders', 'tour-operator' ); ?></a></li>
+		<li><a href="#ui-advanced"><?php esc_html_e( 'Advanced', 'tour-operator' ); ?></a></li>
 	</ul>
 
 	<?php $class = 'active'; ?>
-	<?php if(class_exists('LSX_Banners')) { ?>
-        <div id="ui-general" class="ui-tab <?php echo wp_kses_post($class); ?>">
-            <table class="form-table">
-                <tbody>
-                <?php do_action('lsx_to_framework_display_tab_content','basic'); ?>
-                </tbody>
-            </table>
-        </div>
-    <?php $class = ''; } ?>
 
-	<?php if(class_exists('LSX_Currencies')) { ?>
-		<div id="ui-currencies" class="ui-tab <?php echo wp_kses_post($class); ?>">
+	<?php if ( class_exists( 'LSX_Banners' ) ) { ?>
+		<div id="ui-placeholders" class="ui-tab <?php echo esc_attr( $class ); ?>">
 			<table class="form-table">
 				<tbody>
-				<?php do_action('lsx_to_framework_display_tab_content','currency_switcher'); ?>
+					<?php //do_action( 'lsx_to_framework_display_tab_content', 'placeholders' ); ?>
+					<?php do_action( 'lsx_to_framework_display_tab_content', 'basic' ); ?>
 				</tbody>
 			</table>
 		</div>
 	<?php $class = ''; } ?>
 
-	<?php if(class_exists('LSX_TO_Maps')) { ?>
-		<div id="ui-maps" class="ui-tab <?php echo wp_kses_post($class); ?>">
+	<?php if ( class_exists( 'LSX_Currencies' ) ) { ?>
+		<div id="ui-currencies" class="ui-tab <?php echo esc_attr( $class ) ?>">
 			<table class="form-table">
 				<tbody>
-				<?php do_action('lsx_to_framework_display_tab_content','maps'); ?>
+					<?php do_action( 'lsx_to_framework_display_tab_content', 'currency_switcher' ); ?>
 				</tbody>
 			</table>
 		</div>
 	<?php $class = ''; } ?>
 
-	<?php if(class_exists('LSX_TO_Search')) { ?>
-		<div id="ui-search" class="ui-tab <?php echo wp_kses_post($class); ?>">
+	<?php if ( class_exists( 'LSX_Team' ) ) { ?>
+		<div id="ui-team" class="ui-tab <?php echo esc_attr( $class ) ?>">
 			<table class="form-table">
 				<tbody>
-				<?php do_action('lsx_to_framework_display_tab_content','search'); ?>
+					<?php do_action( 'lsx_to_framework_display_tab_content', 'team' ); ?>
+				</tbody>
+			</table>
+		</div>
+	<?php } ?>
+
+	<?php if ( class_exists( 'LSX_Testimonials' ) ) { ?>
+		<div id="ui-testimonials" class="ui-tab <?php echo esc_attr( $class ) ?>">
+			<table class="form-table">
+				<tbody>
+					<?php do_action( 'lsx_to_framework_display_tab_content', 'testimonials' ); ?>
 				</tbody>
 			</table>
 		</div>
 	<?php $class = ''; } ?>
 
-	<div id="ui-placeholders" class="ui-tab <?php echo wp_kses_post($class); ?>">
+	<?php if ( class_exists( 'LSX_Projects' ) ) { ?>
+		<div id="ui-projects" class="ui-tab <?php echo esc_attr( $class ) ?>">
+			<table class="form-table">
+				<tbody>
+					<?php do_action( 'lsx_to_framework_display_tab_content', 'projects' ); ?>
+				</tbody>
+			</table>
+		</div>
+	<?php $class = ''; } ?>
+
+	<?php if ( class_exists( 'LSX_Blog_Customizer' ) ) { ?>
+		<div id="ui-blog-customizer" class="ui-tab <?php echo esc_attr( $class ) ?>">
+			<table class="form-table">
+				<tbody>
+					<?php do_action( 'lsx_to_framework_display_tab_content', 'blog-customizer' ); ?>
+				</tbody>
+			</table>
+		</div>
+	<?php $class = ''; } ?>
+
+	<?php if ( class_exists( 'LSX_Sharing' ) ) { ?>
+		<div id="ui-sharing" class="ui-tab <?php echo esc_attr( $class ) ?>">
+			<table class="form-table">
+				<tbody>
+					<?php do_action( 'lsx_to_framework_display_tab_content', 'sharing' ); ?>
+				</tbody>
+			</table>
+		</div>
+	<?php $class = ''; } ?>
+
+	<?php if ( class_exists( 'LSX_TO_Maps' ) ) { ?>
+		<div id="ui-maps" class="ui-tab <?php echo esc_attr( $class ); ?>">
+			<table class="form-table">
+				<tbody>
+					<?php do_action( 'lsx_to_framework_display_tab_content', 'maps' ); ?>
+				</tbody>
+			</table>
+		</div>
+	<?php $class = ''; } ?>
+
+	<?php if ( class_exists( 'LSX_TO_Search' ) ) { ?>
+		<div id="ui-search" class="ui-tab <?php echo esc_attr( $class ); ?>">
+			<table class="form-table">
+				<tbody>
+					<?php do_action( 'lsx_to_framework_display_tab_content', 'search' ); ?>
+				</tbody>
+			</table>
+		</div>
+	<?php $class = ''; } ?>
+
+	<div id="ui-placeholders" class="ui-tab <?php echo esc_attr( $class ); ?>">
 		<table class="form-table">
 			<tbody>
-			<?php do_action('lsx_to_framework_display_tab_content','placeholders'); ?>
+				<?php do_action( 'lsx_to_framework_display_tab_content', 'placeholders' ); ?>
 			</tbody>
 		</table>
 	</div>
@@ -69,11 +146,11 @@
 	<div id="ui-advanced" class="ui-tab">
 		<table class="form-table">
 			<tbody>
-			<?php do_action('lsx_to_framework_display_tab_content','advanced'); ?>
+				<?php do_action( 'lsx_to_framework_display_tab_content', 'advanced' ); ?>
 			</tbody>
 		</table>
 	</div>
 
-	<?php do_action('lsx_to_framework_display_tab_bottom','display'); ?>
+	<?php do_action( 'lsx_to_framework_display_tab_bottom', 'display' ); ?>
 
 </div>
