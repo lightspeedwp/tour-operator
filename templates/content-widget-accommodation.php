ff2e6591bce20a32f78434231e2ac5e7012660ad
Fernando Dalpiaz <fmdalpiaz@gmail.com>
2017-06-14 18:14:07 -0300

    Accommodation widget

--- templates/content-widget-accommodation.php
+++ templates/content-widget-accommodation.php
@@ -9,27 +9,32 @@
 global $disable_placeholder;
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
  	<?php if ( '1' !== $disable_placeholder && true !== $disable_placeholder ) { ?>
-		<div class="thumb">
+		<div class="lsx-to-widget-thumb">
 			<a href="<?php the_permalink(); ?>">
 				<?php lsx_thumbnail( 'lsx-thumbnail-single' ); ?>
 			</a>
 		</div>
 	<?php } ?>
 
-	<h4 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+	<div class="lsx-to-widget-content">
+		<h4 class="lsx-to-widget-title text-center"><?php the_title(); ?></h4>
 
-	<div class="widget-content">
-		<?php lsx_to_price('<div class="meta info"><span class="price">'.esc_html__('From price','tour-operator').': ','</span></div>'); ?>
-		<?php the_terms( get_the_ID(), 'travel-style', '<div class="meta travel-style">'.esc_html__('Accommodation Style','tour-operator').': ', ', ', '</div>' ); ?>
-		<?php the_terms( get_the_ID(), 'accommodation-brand', '<div class="meta accommodation-brand">'.esc_html__('Brand','tour-operator').': ', ', ', '</div>' ); ?>
-		<?php lsx_to_connected_destinations('<div class="meta destination">'.esc_html__('Location','tour-operator').': ','</div>'); ?>
-		<?php lsx_to_accommodation_room_total('<div class="meta rooms">'.esc_html__('Rooms','tour-operator').': <span>','</span></div>'); ?>
-		<?php lsx_to_accommodation_rating('<div class="meta rating">'.esc_html__('Rating','tour-operator').': ','</div>'); ?>
-		<div class="view-more" style="text-align:center;">
-			<a href="<?php the_permalink(); ?>" class="btn btn-primary text-center"><?php esc_html_e('View Accommodation','tour-operator'); ?></a>
+		<div class="lsx-to-widget-meta-data">
+			<?php
+				$meta_class = 'lsx-to-widget-meta lsx-to-widget-meta-';
+
+				lsx_to_price( '<span class="'. $meta_class .'price">'. esc_html__( 'From price', 'tour-operator' ) .': ', '</span>' );
+				the_terms( get_the_ID(), 'travel-style', '<span class="'. $meta_class .'style">'. esc_html__( 'Style', 'tour-operator' ) .': ', ', ', '</span>' );
+				the_terms( get_the_ID(), 'accommodation-brand', '<span class="'. $meta_class .'brand">' .esc_html__( 'Brand', 'tour-operator' ) .': ', ', ', '</span>' );
+				lsx_to_connected_destinations('<span class="'. $meta_class .'destinations">'. esc_html__( 'Location', 'tour-operator' ) .': ', '</span>' );
+				lsx_to_accommodation_room_total('<span class="'. $meta_class .'rooms">' .esc_html__( 'Rooms', 'tour-operator' ) .': ', '</span>' );
+				lsx_to_accommodation_rating('<span class="'. $meta_class .'rating">'. esc_html__( 'Rating', 'tour-operator' ) .': ', '</span>' );
+			?>
 		</div>
-	</div>
 
+		<p>
+			<a href="<?php the_permalink(); ?>" class="moretag"><?php esc_html_e( 'View tours', 'tour-operator' ); ?></a>
+		</p>
+	</div>
 </article>
