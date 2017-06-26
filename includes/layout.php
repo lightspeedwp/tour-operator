bae42525ff0343787bce26ac8e22a243493da048
Fernando Tessmann <nandotess85@gmail.com>
2017-06-26 11:02:54 -0300

    fixed image size on layout.php

--- includes/layout.php
+++ includes/layout.php
@@ -46,7 +46,7 @@ function lsx_to_archive_entry_top() {
 	if ( in_array( get_post_type(), array_keys( lsx_to_get_post_types() ) ) && ( is_archive() || $lsx_to_archive ) ) { ?>
 		<?php
 			$thumbnail_id = get_post_thumbnail_id( get_the_ID() );
-			$image_arr = wp_get_attachment_image_src( $thumbnail_id, 'lsx-single-thumbnail' );
+			$image_arr = wp_get_attachment_image_src( $thumbnail_id, 'lsx-thumbnail-single' );
 
 			if ( is_array( $image_arr ) ) {
 				$image_src = $image_arr[0];
