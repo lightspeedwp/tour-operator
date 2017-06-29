b9bbad4b3a8b41e515b024bfb5e1b0dd255a52fc
Fernando Dalpiaz <fmdalpiaz@gmail.com>
2017-06-29 15:03:09 -0300

    Tour single Read More button

--- templates/content-tour.php
+++ templates/content-tour.php
@@ -21,20 +21,19 @@ if ( 1 !== $lsx_to_archive ) {
 	<?php if ( is_single() && false === $lsx_to_archive ) { ?>
 
 		<div <?php lsx_to_entry_class( 'entry-content' ); ?>>
-			<div class="single-main-info">
-				<h3><?php esc_html_e( 'Summary' , 'tour-operator' ); ?></h3>
+			<div class="lsx-to-single-content">
+				<h2 class="lsx-to-single-title"><?php esc_html_e( 'Summary' , 'tour-operator' ); ?></h2>
 
-				<div class="meta info">
+				<div class="lsx-to-single-meta">
 					<?php
-						lsx_to_price( '<span class="price">' . esc_html__( 'From price', 'tour-operator' ) . ': ', '</span>' );
-						lsx_to_duration( '<span class="duration">' . esc_html__( 'Duration', 'tour-operator' ) . ': ', '</span>' );
+						lsx_to_price( '<span class="lsx-to-meta-data lsx-to-meta-data-price">' . esc_html__( 'From price', 'tour-operator' ) . ': ', '</span>' );
+						lsx_to_duration( '<span class="lsx-to-meta-data lsx-to-meta-data-duration">' . esc_html__( 'Duration', 'tour-operator' ) . ': ', '</span>' );
 					?>
 				</div>
-
-				<?php lsx_to_sharing(); ?>
 			</div>
 
 			<?php the_content(); ?>
+			<?php lsx_to_sharing(); ?>
 		</div>
 
 	<?php } elseif ( empty( tour_operator()->options[ get_post_type() ]['disable_entry_text'] ) ) { ?>
