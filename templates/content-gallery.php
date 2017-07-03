1fb36a9c7a5753578a1f8319e0440952887a776f
Fernando Tessmann <nandotess85@gmail.com>
2017-07-03 13:12:32 -0300

    disable single per entry/post

--- templates/content-gallery.php
+++ templates/content-gallery.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * Team Content Part
- * 
+ *
  * @package 	tour-operator
  * @category	team
  */
-global $image,$size;
-$srcset = wp_get_attachment_image_src($image->ID,$size);
-if(false !== $srcset){
-?>
-	<figure id="post-<?php the_ID(); ?>">
-		<?php echo wp_kses_post( apply_filters( 'lsx_to_lazyload_filter_images', '<img title="'. apply_filters('the_title',$image->post_title) .'" src="'. $srcset[0] .'" />' ) ); ?>
+global $image, $size;
+$srcset = wp_get_attachment_image_src( $image->ID, $size );
+
+if ( false !== $srcset ) { ?>
+	<figure>
+		<?php echo wp_kses_post( apply_filters( 'lsx_to_lazyload_filter_images', '<img title="' . apply_filters( 'the_title', $image->post_title ) . '" src="' . $srcset[0] . '" />' ) ); ?>
 	</figure>
 <?php } ?>
--- templates/content-widget-accommodation-brand.php
+++ templates/content-widget-accommodation-brand.php
@@ -11,17 +11,17 @@ global $term, $taxonomy, $disable_placeholder;
 
 $title_link = esc_url( get_term_link( $term, $taxonomy ) );
 ?>
-<article id="term-<?php echo esc_attr( $term->term_id ); ?>" class="term-<?php echo esc_attr( $term->term_id ); ?> <?php echo esc_attr( $taxonomy ); ?> type-<?php echo esc_attr( $taxonomy ); ?> status-publish hentry">
- 	<?php if ( empty( $disable_placeholder ) ) { ?>
+<article class="term-<?php echo esc_attr( $term->term_id ); ?> <?php echo esc_attr( $taxonomy ); ?> type-<?php echo esc_attr( $taxonomy ); ?> status-publish hentry">
+	<?php if ( empty( $disable_placeholder ) ) { ?>
 		<?php if ( lsx_to_has_term_thumbnail( $term->term_id ) ) : ?>
 			<div class="lsx-to-widget-thumb">
-				<a href="<?php echo $title_link; ?>">
+				<a href="<?php echo esc_url( $title_link ); ?>">
 					<?php lsx_to_term_thumbnail( $term->term_id, 'lsx-thumbnail-single' ); ?>
 				</a>
 			</div>
-		<?php else: ?>
+		<?php else : ?>
 			<div class="lsx-to-widget-thumb">
-				<a href="<?php echo $title_link; ?>">
+				<a href="<?php echo esc_url( $title_link ); ?>">
 					<img alt="Placeholder" class="attachment-responsive wp-post-image lsx-responsive" src="<?php echo esc_attr( lsx\legacy\Placeholders::placeholder_url( null, null, array( 750, 350 ) ) ); ?>">
 				</a>
 			</div>
@@ -29,10 +29,10 @@ $title_link = esc_url( get_term_link( $term, $taxonomy ) );
 	<?php } ?>
 
 	<div class="lsx-to-widget-content">
-		<h4 class="lsx-to-widget-title text-center"><a href="<?php echo $title_link; ?>"><?php echo esc_html( apply_filters( 'the_title', $term->name ) ); ?></a></h4>
+		<h4 class="lsx-to-widget-title text-center"><a href="<?php echo esc_url( $title_link ); ?>"><?php echo esc_html( apply_filters( 'the_title', $term->name ) ); ?></a></h4>
 
 		<p>
-			<a href="<?php echo $title_link; ?>" class="moretag"><?php esc_html_e( 'View accommodation', 'tour-operator' ); ?></a>
+			<a href="<?php echo esc_url( $title_link ); ?>" class="moretag"><?php esc_html_e( 'View accommodation', 'tour-operator' ); ?></a>
 		</p>
 	</div>
 </article>
