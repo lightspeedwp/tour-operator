6d061f35b041d9ad76bfed92f8b6b3c7c2d0b1e5
Warwick Booth <warwick@lsdev.biz>
2017-08-15 23:59:33 +0200

    Updated the API setting tab hide the "settings" tab if certain plugins are not active. Fixed the selecting of the API class if the license manager is not active. Fixed the output of the 2 messages.

--- includes/partials/api.php
+++ includes/partials/api.php
@@ -1,32 +1,38 @@
 <div class="uix-field-wrapper">
 
+	<?php
+		$display_settings_page = false;
+		if ( class_exists( 'TO_Maps' ) || class_exists( 'LSX_Currencies' ) || class_exists( 'WETU_Importer' ) ) {
+			$display_settings_page = true;
+		}
+	?>
+
+
 	<ul class="ui-tab-nav">
-		<li><a href="#ui-settings" class="active"><?php esc_html_e('Settings','tour-operator'); ?></a></li>
-		<li><a href="#ui-keys"><?php esc_html_e('License Keys','tour-operator'); ?></a></li>
+		<?php if ( false !== $display_settings_page ) { ?><li><a href="#ui-settings" class="active"><?php esc_html_e('Settings','tour-operator'); ?></a></li><?php } ?>
+		<li><a href="#ui-keys" <?php if ( false === $display_settings_page ) { ?>class="active"<?php } ?>><?php esc_html_e('License Keys','tour-operator'); ?></a></li>
 	</ul>
 
-	<div id="ui-settings" class="ui-tab active">
-        <p>Settings</p>
-		<table class="form-table" style="margin-top:-13px !important;">
-			<tbody>
-			<?php do_action('lsx_to_framework_api_tab_content','settings'); ?>
-			</tbody>
-		</table>
-	</div>
+	<?php if ( false !== $display_settings_page ) { ?>
+		<div id="ui-settings" class="ui-tab active">
+			<p>Settings</p>
+			<table class="form-table" style="margin-top:-13px !important;">
+				<tbody>
+				<?php do_action('lsx_to_framework_api_tab_content','settings'); ?>
+				</tbody>
+			</table>
+		</div>
+	<?php } ?>
 
-	<div id="ui-keys" class="ui-tab">
+	<div id="ui-keys" class="ui-tab <?php if ( false === $display_settings_page ) { ?>active<?php } ?>">
 		<table class="form-table" style="margin-top:-13px !important;">
 			<tbody>
 			<?php
-			$api_keys_content = false;
-			ob_start();
-			do_action('lsx_to_framework_api_tab_content','api');
-			$api_keys_content = ob_end_clean();
-			if(false !== $api_keys_content){
-				?><p class="info"><?php esc_html_e('Enter the license keys for your add-ons in the boxes below.','tour-operator'); ?></p><?php
+			if ( class_exists( 'LSX_API_Manager') ) {
+				?><p class="info"><?php esc_html_e('Enter the license keys for your add-ons in the boxes below.','tour-operator' ); ?></p><?php
 				do_action('lsx_to_framework_api_tab_content','api');
-			}else{ ?>
-				<p class="info"><?php esc_html_e('You have not installed any add-ons yet. View our list of add-ons','tour-operator'); ?> <a href="<?php echo esc_url(admin_url('admin.php')); ?>?page=to-addons"><?php esc_html_e('here','tour-operator'); ?></a>.</p>
+			} else { ?>
+				<p class="info"><?php esc_html_e( 'You have not installed any add-ons yet. View our list of add-ons', 'tour-operator' ); ?> <a href="<?php echo esc_url(admin_url('admin.php')); ?>?page=to-addons"><?php esc_html_e( 'here', 'tour-operator' ); ?></a>.</p>
 			<?php }	?>
 			</tbody>
 		</table>
