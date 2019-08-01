a713b7064f2cab80f580ef1cf9ff5319f5648e8f
JosephHart <joe@lsdev.biz>
2019-08-01 18:02:56 +0200

    Adding the schema code for the tour

--- includes/classes/legacy/class-schema.php
+++ includes/classes/legacy/class-schema.php
@@ -20,23 +20,28 @@ namespace lsx\legacy;
 class Schema {
 
 	/**
-	 * Holds instances of the class
-	 */
+	* Holds instances of the class
+	*/
 	protected static $instance;
 
 	/**
-	 * Constructor
-	 */
+	* Constructor
+	*/
 	public function __construct() {
-		add_action( 'wp_head', array( $this, 'my_schema' ), 1499 );
+		add_action( 'wp_head', array( $this, 'tour_single_schema' ), 1499 );
+		add_action( 'wp_head', array( $this, 'destination_single_schema' ), 1499 );
+		add_action( 'wp_head', array( $this, 'accommodation_single_schema' ), 1499 );
+		add_action( 'wp_head', array( $this, 'reviews_single_schema' ), 1499 );
+		add_action( 'wp_head', array( $this, 'specials_single_schema' ), 1499 );
+		add_action( 'wp_head', array( $this, 'team_single_schema' ), 1499 );
 	}
 
 	/**
-	 * Return an instance of this class.
-	 *
-	 * @since 1.0.0
-	 * @return    object    A single instance of this class.
-	 */
+	* Return an instance of this class.
+	*
+	* @since 1.0.0
+	* @return    object    A single instance of this class.
+	*/
 	public static function get_instance() {
 		// If the single instance hasn't been set, set it now.
 		if ( is_null( self::$instance ) ) {
@@ -51,34 +56,242 @@ class Schema {
 	 * @since 1.0.0
 	 * @return    object    A single instance of this class.
 	 */
-	public function my_schema() {
-	if ( is_singular( 'accommodation' ) ) {
-	 $meta = array(
-		  "@context" => "http://schema.org",
-		  "@type" => "LocalBusiness",
-		  "name" => "storename",
-		  "image" => "https://staticqa.store.com/wp-content/themes/faf/images/store-logo.png",
-		  "@id" => "id",
-		  "url" => "",
-		  "telephone" => "phone",
-		  "priceRange" => "$1-$20",
-		  "address" => array(
-		    "@type" => "PostalAddress",
-		    "streetAddress" => "address",
-		    "addressLocality" => "storecityaddress",
-		    "postalCode" => "storepostaladdress",
-		    "addressCountry" => "USA"
-		  ),
-		    "geo" => array(
-		    "@type" => "GeoCoordinates",
-		    "latitude" => "storelatitude",
-		    "longitude" => "storelongitude"
-		  )
-	 	 );
+	public function tour_single_schema() {
+		if ( is_singular( 'tour' ) ) {
+			$tours_list = get_post_meta( get_the_ID(), 'itinerary', false );
+			$list_array = array();
+			$url_option = get_the_permalink() . '#itinerary';
+			$i = 0;
+				foreach($tours_list as $day) {
+				$day_title        = $day['title'];
+				$day_description  = $day['description'];
+				$i++;
+				$schema_day       = array(
+					"@type" => "ListItem",
+					"position"=> $i,
+					"item" =>
+					array(
+					"@id" => $url_option,
+					"name" => $day_title,
+					"description" => $day_description,
+				)
+			);
+			$list_array[] = $schema_day;		
+			}
+			$meta = array(
+				array(
+					"@type" => "Trip",
+					"description" => "Description Text Here",
+					"image" => "URL for image goes here - banner",
+					"itinerary" => array(
+					"@type" => "ItemList",
+					"itemListElement" => $list_array,
+					),
+					"name" => "Title of tour post",
+					"provider" => "Southern Destinations",
+					"url" => "the page url"
+				),
+			);
 			$output = json_encode( $meta, JSON_UNESCAPED_SLASHES  );
 			?>
 			<script type="application/ld+json">
-			<?php echo $output; ?>
+				<?php echo $output; ?>
+			</script>
+			<?php
+		}
+	}
+
+
+		/**
+	 * Creates the schema for the destination post type
+	 *
+	 * @since 1.0.0
+	 * @return    object    A single instance of this class.
+	 */
+	public function destination_single_schema() {
+		if ( is_singular( 'destination' ) ) {
+			$meta = array(
+				"@context" => "http://schema.org",
+				"@type" => "LocalBusiness",
+				"name" => "storename",
+				"image" => "https://staticqa.store.com/wp-content/themes/faf/images/store-logo.png",
+				"@id" => "id",
+				"url" => "",
+				"telephone" => "phone",
+				"priceRange" => "$1-$20",
+				"address" => array(
+				"@type" => "PostalAddress",
+				"streetAddress" => "address",
+				"addressLocality" => "storecityaddress",
+				"postalCode" => "storepostaladdress",
+				"addressCountry" => "USA"
+				),
+				"geo" => array(
+				"@type" => "GeoCoordinates",
+				"latitude" => "storelatitude",
+				"longitude" => "storelongitude"
+				),
+			);
+			$output = json_encode( $meta, JSON_UNESCAPED_SLASHES  );
+			?>
+			<script type="application/ld+json">
+				<?php echo $output; ?>
+			</script>
+			<?php
+		}
+	}
+
+		/**
+	 * Creates the schema for the accommodation post type
+	 *
+	 * @since 1.0.0
+	 * @return    object    A single instance of this class.
+	 */
+	public function accommodation_single_schema() {
+		if ( is_singular( 'accommodation' ) ) {
+			$meta = array(
+				"@context" => "http://schema.org",
+				"@type" => "LocalBusiness",
+				"name" => "storename",
+				"image" => "https://staticqa.store.com/wp-content/themes/faf/images/store-logo.png",
+				"@id" => "id",
+				"url" => "",
+				"telephone" => "phone",
+				"priceRange" => "$1-$20",
+				"address" => array(
+				"@type" => "PostalAddress",
+				"streetAddress" => "address",
+				"addressLocality" => "storecityaddress",
+				"postalCode" => "storepostaladdress",
+				"addressCountry" => "USA"
+				),
+				"geo" => array(
+				"@type" => "GeoCoordinates",
+				"latitude" => "storelatitude",
+				"longitude" => "storelongitude"
+				),
+			);
+			$output = json_encode( $meta, JSON_UNESCAPED_SLASHES  );
+			?>
+			<script type="application/ld+json">
+				<?php echo $output; ?>
+			</script>
+			<?php
+		}
+	}
+
+		/**
+	 * Creates the schema for the reviews post type
+	 *
+	 * @since 1.0.0
+	 * @return    object    A single instance of this class.
+	 */
+	public function reviews_single_schema() {
+		if ( is_singular( 'review' ) ) {
+			$meta = array(
+				"@context" => "http://schema.org",
+				"@type" => "LocalBusiness",
+				"name" => "storename",
+				"image" => "https://staticqa.store.com/wp-content/themes/faf/images/store-logo.png",
+				"@id" => "id",
+				"url" => "",
+				"telephone" => "phone",
+				"priceRange" => "$1-$20",
+				"address" => array(
+				"@type" => "PostalAddress",
+				"streetAddress" => "address",
+				"addressLocality" => "storecityaddress",
+				"postalCode" => "storepostaladdress",
+				"addressCountry" => "USA"
+				),
+				"geo" => array(
+				"@type" => "GeoCoordinates",
+				"latitude" => "storelatitude",
+				"longitude" => "storelongitude"
+				),
+			);
+			$output = json_encode( $meta, JSON_UNESCAPED_SLASHES  );
+			?>
+			<script type="application/ld+json">
+				<?php echo $output; ?>
+			</script>
+			<?php
+		}
+	}
+
+		/**
+	 * Creates the schema for the specials post type
+	 *
+	 * @since 1.0.0
+	 * @return    object    A single instance of this class.
+	 */
+	public function specials_single_schema() {
+		if ( is_singular( 'special' ) ) {
+			$meta = array(
+				"@context" => "http://schema.org",
+				"@type" => "LocalBusiness",
+				"name" => "storename",
+				"image" => "https://staticqa.store.com/wp-content/themes/faf/images/store-logo.png",
+				"@id" => "id",
+				"url" => "",
+				"telephone" => "phone",
+				"priceRange" => "$1-$20",
+				"address" => array(
+				"@type" => "PostalAddress",
+				"streetAddress" => "address",
+				"addressLocality" => "storecityaddress",
+				"postalCode" => "storepostaladdress",
+				"addressCountry" => "USA"
+				),
+				"geo" => array(
+				"@type" => "GeoCoordinates",
+				"latitude" => "storelatitude",
+				"longitude" => "storelongitude"
+				),
+			);
+			$output = json_encode( $meta, JSON_UNESCAPED_SLASHES  );
+			?>
+			<script type="application/ld+json">
+				<?php echo $output; ?>
+			</script>
+			<?php
+		}
+	}
+
+	/**
+	 * Creates the schema for the team post type
+	 *
+	 * @since 1.0.0
+	 * @return    object    A single instance of this class.
+	 */
+	public function team_single_schema() {
+		if ( is_singular( 'team' ) ) {
+			$meta = array(
+				"@context" => "http://schema.org",
+				"@type" => "LocalBusiness",
+				"name" => "storename",
+				"image" => "https://staticqa.store.com/wp-content/themes/faf/images/store-logo.png",
+				"@id" => "id",
+				"url" => "",
+				"telephone" => "phone",
+				"priceRange" => "$1-$20",
+				"address" => array(
+				"@type" => "PostalAddress",
+				"streetAddress" => "address",
+				"addressLocality" => "storecityaddress",
+				"postalCode" => "storepostaladdress",
+				"addressCountry" => "USA"
+				),
+				"geo" => array(
+				"@type" => "GeoCoordinates",
+				"latitude" => "storelatitude",
+				"longitude" => "storelongitude"
+				),
+			);
+			$output = json_encode( $meta, JSON_UNESCAPED_SLASHES  );
+			?>
+			<script type="application/ld+json">
+				<?php echo $output; ?>
 			</script>
 			<?php
 		}
