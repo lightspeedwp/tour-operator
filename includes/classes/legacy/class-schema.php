4643214d8c79d28556a5111937a36495b56d7a04
JosephHart <joe@lsdev.biz>
2019-08-07 12:05:26 +0200

    Removing the reviews and specials and adding to respective extensions

--- includes/classes/legacy/class-schema.php
+++ includes/classes/legacy/class-schema.php
@@ -31,8 +31,6 @@ class Schema {
 		add_action( 'wp_head', array( $this, 'tour_single_schema' ), 1499 );
 		add_action( 'wp_head', array( $this, 'destination_single_schema' ), 1499 );
 		add_action( 'wp_head', array( $this, 'accommodation_single_schema' ), 1499 );
-		add_action( 'wp_head', array( $this, 'reviews_single_schema' ), 1499 );
-		add_action( 'wp_head', array( $this, 'specials_single_schema' ), 1499 );
 		add_action( 'wp_head', array( $this, 'team_single_schema' ), 1499 );
 	}
 
@@ -276,121 +274,6 @@ class Schema {
 					),
 			);
 			$output = wp_json_encode( $meta, JSON_UNESCAPED_SLASHES );
-			?>
-			<script type="application/ld+json">
-				<?php echo wp_kses_post( $output ); ?>
-			</script>
-			<?php
-		}
-	}
-
-	/**
-	 * Creates the schema for the reviews post type
-	 *
-	 * @since 1.0.0
-	 * @return    object    A single instance of this class.
-	 */
-	public function reviews_single_schema() {
-		if ( is_singular( 'review' ) ) {
-
-			$rating_value = get_post_meta( get_the_ID(), 'rating', true );
-			$review_author = get_post_meta( get_the_ID(), 'reviewer_name', false );
-			$title_accommodation = get_the_title();
-			$url_accommodation = get_the_permalink();
-			$review_description = wp_strip_all_tags( get_the_content() );
-			$review_thumb = get_the_post_thumbnail_url(get_the_ID(),'full');
-			$review_email = get_post_meta( get_the_ID(), 'reviewer_email', false );
-
-
-			$meta = array(
-			"@context" => "https://schema.org/",
-			"@type" => "Service",
-			"description" => $review_description,
-			"image" => $review_thumb,
-			"name" => $review_title,
-			"review" => array(
-				"@type" => "Review",
-				"reviewRating" => array(
-				"@type" => "Rating",
-				"ratingValue" => $rating_value,
-				),
-				"author" => array(
-				"@type" => "Person",
-				"name" => $review_author,
-				"email" => $review_email
-				),
-				"reviewBody" => $review_description
-			),
-			"aggregateRating" => array(
-				"@type" => "AggregateRating",
-				"ratingValue" => $rating_value,
-				"bestRating"  => $rating_value,
-				"ratingCount" => "1"
-			),
-			);
-			$output = wp_json_encode( $meta, JSON_UNESCAPED_SLASHES  );
-			?>
-			<script type="application/ld+json">
-				<?php echo wp_kses_post( $output ); ?>
-			</script>
-			<?php
-		}
-	}
-
-
-	/**
-	 * Creates the schema for the specials post type
-	 *
-	 * @since 1.0.0
-	 * @return    object    A single instance of this class.
-	 */
-	public function specials_single_schema() {
-		if ( is_singular( 'special' ) ) {
-
-		$destination_list_special = get_post_meta( get_the_ID(), 'destination_to_special', false );
-		$destination_list_schema = array();
-		$url_option = get_the_permalink();
-		$special_title = get_the_title();
-		$primary_url = get_the_permalink();
-		$special_content = wp_strip_all_tags( get_the_content() );
-		$thumb_url = get_the_post_thumbnail_url( get_the_ID(), 'full' );
-		$price = get_post_meta( get_the_ID(), 'price', false );
-		$start_validity = get_post_meta( get_the_ID(), 'booking_validity_start', false );
-		$end_validity = get_post_meta( get_the_ID(), 'booking_validity_end', false );
-	
-
-		if ( ! empty( $destination_list_special ) ) {
-			foreach( $destination_list_special as $single_destination ) {
-				$url_option   = get_the_permalink() . '#destination-' . $i;
-				$destination_name = get_the_title($single_destination);
-				$schema_day       = array(
-				"@type" => "PostalAddress",
-				"addressLocality" => $destination_name,
-			);
-				$destination_list_schema[] = $schema_day;
-				}
-			}
-			$meta = array(
-				array(
-					"@context" => "http://schema.org",
-					"@type" => array("Trip", "ProfessionalService", "Offer"),
-					"offers" => array(
-					"@type" => "Offer",
-					"price" => $price,
-					"availabilityStarts" => $start_validity,
-					"availabilityEnds" => $end_validity,
-					),
-					"address" => $destination_list_schema,
-					"telephone" => "0216713090",
-					"priceRange" => $price,
-					"description" => $special_content,
-					"image" => $thumb_url,
-					"name" => $special_title,
-					"provider" => "Southern Destinations",
-					"url" => $primary_url,
-				),
-			);
-			$output = wp_json_encode( $meta, JSON_UNESCAPED_SLASHES  );
 			?>
 			<script type="application/ld+json">
 				<?php echo wp_kses_post( $output ); ?>
