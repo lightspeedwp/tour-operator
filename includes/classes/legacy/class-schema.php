04d27c7a6a1536835e86fb111d63a37298cfeac1
JosephHart <joe@lsdev.biz>
2019-08-07 12:28:54 +0200

    Removing team schema

--- includes/classes/legacy/class-schema.php
+++ includes/classes/legacy/class-schema.php
@@ -31,7 +31,6 @@ class Schema {
 		add_action( 'wp_head', array( $this, 'tour_single_schema' ), 1499 );
 		add_action( 'wp_head', array( $this, 'destination_single_schema' ), 1499 );
 		add_action( 'wp_head', array( $this, 'accommodation_single_schema' ), 1499 );
-		add_action( 'wp_head', array( $this, 'team_single_schema' ), 1499 );
 	}
 
 	/**
@@ -279,49 +278,6 @@ class Schema {
 				<?php echo wp_kses_post( $output ); ?>
 			</script>
 			<?php
-		}
-	}
-
-
-	/**
-	 * Creates the schema for the team post type
-	 *
-	 * @since 1.0.0
-	 * @return    object    A single instance of this class.
-	 */
-	public function team_single_schema() {
-		if ( is_singular( 'team' ) ) {
-			$pinterest = get_post_meta( get_the_ID(), 'pinterest', true );
-			$googleplus = get_post_meta( get_the_ID(), 'googleplus', true );
-			$twitter = get_post_meta( get_the_ID(), 'twitter', true );
-			$linkedin = get_post_meta( get_the_ID(), 'linkedin', true );
-			$facebook = get_post_meta( get_the_ID(), 'facebook', true );
-			$phone_number_team = get_post_meta( get_the_ID(), 'contact_number', true );
-			$job_role_team = get_post_meta( get_the_ID(), 'role', true );
-			$email_team = get_post_meta( get_the_ID(), 'contact_email', true );
-			$name_team = get_the_title();
-			$team_description = wp_strip_all_tags( get_the_content() );
-
-		if (! empty($googleplus) || ! empty($pinterest) || ! empty($twitter) || ! empty($linkedin) ||  ! empty($facebook) ) {
-			$social_array = array( $googleplus, $pinterest, $twitter, $linkedin, $facebook );
-		}
-
-		$meta = array(
-		"@context" => "http://schema.org/",
-		"@type" => "Person",
-		"email" => $email_team,
-		"jobTitle" => $job_role_team,
-		"telephone" => $phone_number_team,
-		"description" => $team_description,
-		"name" => $name_team,
-		"sameAs" => $social_array,
-		);
-		$output = wp_json_encode( $meta, JSON_UNESCAPED_SLASHES  );
-		?>
-		<script type="application/ld+json">
-			<?php echo wp_kses_post( $output ); ?>
-		</script>
-		<?php
 		}
 	}
 }
