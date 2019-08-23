1133c06f66eb99273134757c548e6d58b6a82ad4
Warwick Booth <warwick@lsdev.biz>
2019-08-23 08:32:36 +0200

    changing the accommodation fields

--- includes/classes/legacy/schema/class-schema-accommodation.php
+++ includes/classes/legacy/schema/class-schema-accommodation.php
@@ -54,10 +54,6 @@ class LSX_TO_Schema_Accommodation implements WPSEO_Graph_Piece {
 	 */
 	public function __construct( WPSEO_Schema_Context $context ) {
 		$this->context          = $context;
-		$this->itinerary_fields = array(
-			'accommodation_to_tour' => 'Accommodation',
-			'destination_to_tour'   => 'State',
-		);
 		$this->place_ids        = array();
 		$this->post             = get_post( $this->context->id );
 		$this->post_url         = get_permalink( $this->context->id );
@@ -91,7 +87,7 @@ class LSX_TO_Schema_Accommodation implements WPSEO_Graph_Piece {
 				'Accommodation',
 				'Product',
 			),
-			'@id'              => $this->context->canonical . '#tour',
+			'@id'              => $this->context->canonical . '#accommodation',
 			'name'             => $this->post->post_title,
 			'description'      => wp_strip_all_tags( $this->post->post_content ),
 			'url'              => $this->post_url,
@@ -110,6 +106,7 @@ class LSX_TO_Schema_Accommodation implements WPSEO_Graph_Piece {
 		if ( false !== $wetu_ref && '' !== $wetu_ref ) {
 			$data['sku'] = $wetu_ref;
 		}
+		$data = $this->add_itinerary( $data );
 		$data = $this->add_image( $data );
 		$data = $this->add_offers( $data );
 		$data = $this->add_reviews( $data );
@@ -154,7 +151,7 @@ class LSX_TO_Schema_Accommodation implements WPSEO_Graph_Piece {
 	 */
 	private function add_itinerary( $data ) {
 		$places       = array();
-		$destinations = get_post_meta( $this->context->id, 'destination_to_tour', false );
+		$destinations = get_post_meta( $this->context->id, 'destination_to_accommodation', false );
 		if ( ! empty( $destinations ) ) {
 			foreach ( $destinations as $destination ) {
 				if ( '' !== $destination ) {
@@ -165,97 +162,11 @@ class LSX_TO_Schema_Accommodation implements WPSEO_Graph_Piece {
 				}
 			}
 			if ( ! empty( $places ) ) {
-				$data['itinerary'] = $places;
+				$data['containedInPlace'] = $places;
 			}
 		}
 
 		return $data;
-	}
-
-	/**
-	 * Adds the itinerary destinations as an itemList
-	 *
-	 * @param array $data Trip data.
-	 *
-	 * @return array $data Trip data.
-	 */
-	private function add_sub_trips( $data ) {
-		/**
-		 * Filter: 'wpseo_schema_tour_sub_trips_meta_key' - Allow changing the custom field meta_key used to assign the Sub Trips to a post type Trip data.
-		 *
-		 * @api string $meta_key The chosen meta_key.
-		 */
-		$meta_key = apply_filters( 'wpseo_schema_tour_sub_trips_meta_key', 'itinerary' );
-		return $this->add_subtrip_days( $data, 'subTrip', $meta_key );
-	}
-
-	/**
-	 * Adds the days of the subTrip to the dta.
-	 *
-	 * @param array  $data     Trip data.
-	 * @param string $key      The key in data to save the terms in.
-	 * @param string $taxonomy The taxonomy to retrieve the terms from.
-	 *
-	 * @return mixed array $data Review data.
-	 */
-	private function add_subtrip_days( $data, $key, $meta_key ) {
-		$itinerary  = get_post_meta( get_the_ID(), 'itinerary', false );
-		$list_array = array();
-		$i          = 0;
-
-		if ( ! empty( $itinerary ) ) {
-			foreach ( $itinerary as $day ) {
-				$i++;
-				$schema = array(
-					'@type'    => 'Trip',
-					'@id'         => $this->get_subtrip_schema_id( $day['title'], $this->context ),
-					'name'        => $day['title'],
-					'description' => wp_strip_all_tags( $day['description'] ),
-				);
-
-				$itinerary_fields = apply_filters( 'lsx_to_schema_tour_sub_trips_additional_fields', $this->itinerary_fields );
-				$places           = $this->add_subtrip_places( $itinerary_fields, $day );
-				if ( ! empty( $places ) ) {
-					$schema['itinerary'] = $places;
-				}
-
-				$list_array[] = $schema;
-			}
-
-			$data[ $key ] = $list_array;
-		}
-		return $data;
-	}
-
-	/**
-	 * Adds in the accommodation and destination Place if found.
-	 *
-	 * @param array  $itinerary_fields The array of fields to loop through.
-	 * @param string $day The itinrary day array to grab the post_ids from.
-	 *
-	 * @return array $palces Places data.
-	 */
-	private function add_subtrip_places( $itinerary_fields, $day ) {
-		$places = array();
-		if ( ! empty( $itinerary_fields ) ) {
-			foreach ( $itinerary_fields as $key => $type ) {
-
-				if ( isset( $day[ $key ] ) && '' !== $day[ $key ] && ! empty( $day[ $key ] ) ) {
-					foreach ( $day[ $key ] as $place_id  ) {
-						if ( '' !== $place_id ) {
-							// Here we are linking the regions to the country.
-							$contained_in = false;
-							$parent_id = wp_get_post_parent_id( $place_id );
-							if ( false !== $parent_id && ! empty( $this->place_ids ) && isset( $this->place_ids[ $parent_id ] ) ) {
-								$contained_in = $this->place_ids[ $parent_id ];
-							}
-							$places = $this->add_place( $places, $type, $place_id, $contained_in );
-						}
-					}
-				}
-			}
-		}
-		return $places;
 	}
 
 	/**
@@ -343,7 +254,7 @@ class LSX_TO_Schema_Accommodation implements WPSEO_Graph_Piece {
 	 * @return array $data
 	 */
 	private function get_special_offers( $data ) {
-		$specials = get_post_meta( $this->context->id, 'special_to_tour', false );
+		$specials = get_post_meta( $this->context->id, 'special_to_accommodation', false );
 		if ( ! empty( $specials ) ) {
 			foreach ( $specials as $special_id ) {
 				$data = $this->get_special_offer( $data, $special_id );
@@ -359,7 +270,7 @@ class LSX_TO_Schema_Accommodation implements WPSEO_Graph_Piece {
 	 * @return array $data
 	 */
 	private function add_reviews( $data ) {
-		$reviews       = get_post_meta( $this->context->id, 'review_to_tour', false );
+		$reviews       = get_post_meta( $this->context->id, 'review_to_accommodation', false );
 		$reviews_array = array();
 		if ( ! empty( $reviews ) ) {
 			$aggregate_value = 1;
@@ -403,7 +314,7 @@ class LSX_TO_Schema_Accommodation implements WPSEO_Graph_Piece {
 	 * @return array $data
 	 */
 	private function add_articles( $data ) {
-		$posts       = get_post_meta( $this->context->id, 'post_to_tour', false );
+		$posts       = get_post_meta( $this->context->id, 'post_to_accommodation', false );
 		$posts_array = array();
 		if ( ! empty( $posts ) ) {
 			foreach ( $posts as $post_id ) {
@@ -518,9 +429,9 @@ class LSX_TO_Schema_Accommodation implements WPSEO_Graph_Piece {
 			'category'           => __( 'Standard', 'tour-operator' ),
 		);
 		$args     = wp_parse_args( $args, $defaults );
-		$args     = apply_filters( 'lsx_to_schema_tour_offer_args', $args );
+		$args     = apply_filters( 'lsx_to_schema_accommodation_offer_args', $args );
 		$offer    = array(
-			'@type' => apply_filters( 'lsx_to_schema_tour_offer_type', 'Offer', $args ),
+			'@type' => apply_filters( 'lsx_to_schema_accommodation_offer_type', 'Offer', $args ),
 		);
 		foreach ( $args as $key => $value ) {
 			if ( false !== $value ) {
@@ -587,19 +498,6 @@ class LSX_TO_Schema_Accommodation implements WPSEO_Graph_Piece {
 	public function get_places_schema_id( $place_id, $type, $context ) {
 		$url                          = $context->site_url . '#/schema/' . strtolower( $type ) . '/' . wp_hash( $place_id . get_the_title( $place_id ) );
 		$this->place_ids[ $place_id ] = $url;
-		return trailingslashit( $url );
-	}
-
-	/**
-	 * Retrieve a users Schema ID.
-	 *
-	 * @param string               $name The Name of the Reviewer you need a for.
-	 * @param WPSEO_Schema_Context $context A value object with context variables.
-	 *
-	 * @return string The user's schema ID.
-	 */
-	public function get_subtrip_schema_id( $name, $context ) {
-		$url = $context->site_url . '#/subtrip/' . wp_hash( $name . $context->id );
 		return trailingslashit( $url );
 	}
 
