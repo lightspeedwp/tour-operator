ec35d963afd8bceed712f39fd5b28340ca55e0a3
Warwick Booth <warwick@lsdev.biz>
2019-10-22 13:35:30 +0200

    Edting the field and the field styles

--- includes/metaboxes/config-tour.php
+++ includes/metaboxes/config-tour.php
@@ -221,12 +221,13 @@ $metabox['fields'][] = array(
 );
 
 $metabox['fields'][] = array(
-	'id'         => 'gallery',
-	'name'       => esc_html__( 'Gallery', 'tour-operator' ),
-	'type'       => 'image',
-	'repeatable' => true,
-	'show_size'  => false,
-	'sortable'   => true,
+	'id'                  => 'gallery',
+	'name'                => '',
+	'type'                => 'image',
+	'repeatable'          => true,
+	'show_size'           => false,
+	'sortable'            => true,
+	'string-repeat-field' => esc_html__( 'Add new image', 'tour-operator' ),
 );
 
 if ( class_exists( 'Envira_Gallery' ) ) {
