6a2a01f215abf336dcb2f98fa776b546eabdc932
Warwick Booth <warwick@lsdev.biz>
2020-11-20 16:24:06 +0200

    Help page styling

--- includes/partials/help.php
+++ includes/partials/help.php
@@ -2,13 +2,14 @@
 	$lsx_to_documentation = esc_url( 'https://tour-operator.lsdev.biz/documentation/' );
 	$extensions_link      = esc_url( 'https://www.lsdev.biz/lsx/extensions/tour-operator/' );
 	$version              = esc_html( LSX_TO_VER );
+	$support_link         = esc_url( 'https://github.com/lightspeeddevelopment/tour-operator/issues' );
 ?>
 
 <div class="wrap about-wrap">
 	<div class="row">
 		<div class="col-md-12 top-header">
-			<h1 class="small"><?php esc_html_e( 'LightSpeed’s LSX Tour Operator Plugin', 'tour-operator' ); ?></h1>
-			<p><?php esc_html_e( 'Thank you for using the LSX Tour Operator plugin. All of us here at LightSpeed appreciate your ongoing support and we can\'t wait to see what people create with the plugin. We\'re committed to ensuring you have all the help you need to make the most of the plugin.', 'tour-operator' ); ?></p>
+			<h1 class="small"><?php esc_html_e( 'LSX Tour Operator Extensions', 'tour-operator' ); ?></h1>
+			<p><?php esc_html_e( 'Extend the functionality of the LSX Tour Operator plugin with one of our many extensions!', 'tour-operator' ); ?></p>
 		</div>
 	</div>
 
@@ -18,14 +19,13 @@
 				<div class="col-md-12">
 					<div class="box support">
 						<h2><?php esc_html_e( 'Getting Support', 'tour-operator' ); ?></h2>
-						<p><?php esc_html_e( 'Our website\'s', 'tour-operator' ); ?> <a href="<?php echo wp_kses_post( $lsx_to_documentation ); ?>" target="_blank"><?php esc_html_e( 'documentation', 'tour-operator' ); ?></a> <?php esc_html_e( 'page is a great place to start to learn how to configure and customize our plugin and its extensions. Here are some links to some of the key settings and modules:', 'tour-operator' ); ?></p>
-
-						<ul>
-							<li><strong><?php esc_html_e( 'General settings:', 'tour-operator' ); ?></strong> <?php esc_html_e( 'Configure the general settings of the plugin as well as some settings per-module.', 'tour-operator' ); ?></li>
-							<li><strong><?php esc_html_e( 'Destinations:', 'tour-operator' ); ?></strong> <?php esc_html_e( 'Create and configure your Destination single posts and archives', 'tour-operator' ); ?></li>
-							<li><strong><?php esc_html_e( 'Accommodations:', 'tour-operator' ); ?></strong> <?php esc_html_e( 'Create and configure your Accommodation single posts and archives', 'tour-operator' ); ?></li>
-							<li><strong><?php esc_html_e( 'Tours:', 'tour-operator' ); ?></strong> <?php esc_html_e( 'Create and configure your Tours single posts and archives', 'tour-operator' ); ?></li>
-						</ul>
+						<p><?php esc_html_e( 'Our website\'s', 'tour-operator' ); ?> <a href="<?php echo wp_kses_post( $lsx_to_documentation ); ?>" target="_blank"><?php esc_html_e( 'documentation', 'tour-operator' ); ?></a> <?php esc_html_e( 'page is a great place to start to learn how to configure and customize our plugin and its extensions.', 'tour-operator' ); ?></p>
+						<div class="more-button">
+							<a href="<?php echo wp_kses_post( $support_link ); ?>" target="_blank" class="button button-tertiary">
+								<?php esc_html_e( 'Get support today', 'tour-operator' ); ?>
+							</a>
+							<br clear="both" />
+						</div>
 					</div>
 				</div>
 			</div>
@@ -45,7 +45,7 @@
 
 						<div class="more-button">
 							<a href="<?php echo wp_kses_post( $tour_operator_link ); ?>" target="_blank" class="button button-primary">
-								<?php esc_html_e( 'Visit plugin website', 'tour-operator' ); ?>
+								<?php esc_html_e( 'Visit on WordPress', 'tour-operator' ); ?>
 							</a>
 							<br clear="both" />
 						</div>
