d1da9b8cd361fcb3768cb662763dba663cb958e9
Warwick <krugazul@gmail.com>
2024-09-13 15:12:31 +0200

    Adding in the unit query fields.

--- assets/js/blocks/variations.js
+++ assets/js/blocks/variations.js
@@ -3,6 +3,7 @@ wp.domReady(() => {
     wp.blocks.registerBlockVariation('core/group', {
         name: 'lsx/itinerary',
         title: 'Itinerary',
+		icon: 'list-view',
         attributes: {
             metadata: {
                 name: 'Itinerary',
@@ -20,6 +21,33 @@ wp.domReady(() => {
         innerBlocks: [
             ['core/paragraph', {
                 placeholder: 'Insert your Itinerary pattern here.',
+				align: 'center'
+            }]
+        ],
+        isDefault: false
+    });
+
+    wp.blocks.registerBlockVariation('core/group', {
+        name: 'lsx/accommodation-units',
+        title: 'Units',
+		icon : 'admin-multisite',
+        attributes: {
+            metadata: {
+                name: 'Units',
+                bindings: {
+                    content: {
+                        source: 'lsx/accommodation-units'
+                    }
+                }
+            },
+            align: 'wide',
+            layout: {
+                type: 'constrained'
+            }
+        },
+        innerBlocks: [
+            ['core/paragraph', {
+                placeholder: 'Insert your Room pattern here.',
 				align: 'center'
             }]
         ],
