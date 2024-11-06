6eda4d702fadd8868b435949d85dda64db255382
Warwick <krugazul@gmail.com>
2024-11-06 21:40:41 +0200

    Removing the unused JS

--- assets/js/blocks/general.js
+++ assets/js/blocks/general.js
@@ -404,112 +404,27 @@ wp.domReady(() => {
 		]
 	});
 
-	// Location Wrapper (Destination to Accommodation)
-	wp.blocks.registerBlockVariation('core/group', {
-		name: 'lsx-tour-operator/destination-to-accommodation',
-		title: 'Destination to Accommodation',
-		category: 'lsx-tour-operator',
-		attributes: {
-			metadata: {
-				name: 'Destination to Accommodation',
-			},
-			className: 'lsx-destination-to-accommodation-wrapper',
-			style: {
-				spacing: {
-					padding: {
-						top: '0',
-						bottom: '0'
-					},
-					blockGap: '5px'
-				}
-			},
-			layout: {
-				type: 'constrained'
-			}
-		},
-		innerBlocks: [
-			['core/group', {
-					style: {
-						spacing: {
-							blockGap: '5px'
-						}
-					},
-					layout: {
-						type: 'flex',
-						flexWrap: 'nowrap',
-						verticalAlignment: 'top'
-					}
-				},
-				[
-					['core/image', {
-						width: '20px',
-						sizeSlug: 'large',
-						url: 'https://tour-operator.lsx.design/wp-content/uploads/2024/09/Typelocation-icon.png',
-						alt: ''
-					}],
-					['core/paragraph', {
-						style: {
-							spacing: {
-								padding: {
-									top: '2px',
-									bottom: '2px'
-								}
-							}
-						},
-						content: '<strong>Location</strong>:'
-					}]
-				]
-			],
-			['core/group', {
-					style: {
-						spacing: {
-							blockGap: '5px',
-							padding: {left: '25px'}
-						}
-					},
-					layout: {
-						type: 'flex',
-						flexWrap: 'nowrap'
-					}
-				},
-				[
-					['core/paragraph', {
-						metadata: {
-							bindings: {
-								content: {
-									source: 'lsx/post-connection',
-									args: {
-										key: 'destination_to_accommodation'
-									}
-								}
-							}
-						},
-						className: 'has-septenary-color has-text-color has-link-color',
-						style: {
-							elements: {
-								link: {
-									color: {
-										text: 'var:preset|color|primary-700',
-										':hover': {
-											color: {
-												text: 'var:preset|color|primary-900'
-											}
-										}
-									}
-								}
-							},
-							spacing: {
-								padding: {
-									top: '2px',
-									bottom: '2px'
-								}
-							}
-						},
-						textColor: 'primary-700'
-					}]
-				]
-			]
-		]
-	});
-  });
+	wp.blocks.registerBlockVariation('core/button', {
+        name: 'lsx-tour-operator/read-more',
+        title: 'Read More Button',
+        attributes: {
+            className: 'wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex',
+        },
+        innerBlocks: [
+            {
+                name: 'core/button',
+                attributes: {
+                    className: 'lsx-to-more-link more-link has-custom-width wp-block-button__width-100',
+                    style: {
+                        borderBottomLeftRadius: '8px',
+                        borderBottomRightRadius: '8px',
+                    },
+                    backgroundColor: 'primary',
+                    text: 'View More',
+                },
+            },
+        ],
+    });
+
+});
   
