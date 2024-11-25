9847abf0c6676213a851283d5e64dcaaca649be9
Warwick <krugazul@gmail.com>
2024-11-25 21:55:30 +0200

    Removing the unused frontend functions.

--- includes/classes/admin/class-setup.php
+++ includes/classes/admin/class-setup.php
@@ -20,14 +20,6 @@ namespace lsx\admin;
 class Setup {
 
 	/**
-	 * Holds instance of the class
-	 *
-	 * @since   1.1.0
-	 * @var     \lsx\admin\Setup
-	 */
-	private static $instance;
-
-	/**
 	 * Holds the tour operators options
 	 *
 	 * @since   1.1.0
@@ -56,24 +48,39 @@ class Setup {
 			'accommodation',
 			'destination',
 		);
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
 		add_action( 'init', array( $this, 'register_meta_with_rest' ) );
 		add_action( 'cmb2_admin_init', array( $this, 'register_cmb2_fields' ) );
+
+		// Allow extra tags and attributes to wp_kses_post().
+		add_filter(
+			'wp_kses_allowed_html',
+			array(
+				$this,
+				'wp_kses_allowed_html',
+			),
+			10,
+			2
+		);
+		// Allow extra protocols to wp_kses_post().
+		add_filter(
+			'kses_allowed_protocols',
+			array(
+				$this,
+				'kses_allowed_protocols',
+			)
+		);
+		// Allow extra style attributes to wp_kses_post().
+		add_filter( 'safe_style_css', array( $this, 'safe_style_css' ) );
 	}
 
 	/**
-	 * Return an instance of this class.
+	 * Load the plugin text domain for translation.
 	 *
-	 * @since 1.1.0
-	 * @return  Tour_Operator  A single instance
+	 * @since 0.0.1
 	 */
-	public static function init() {
-
-		// If the single instance hasn't been set, set it now.
-		if ( ! isset( self::$instance ) ) {
-			self::$instance = new self();
-		}
-
-		return self::$instance;
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain( 'tour-operator', false, basename( LSX_TO_PATH ) . '/languages' );
 	}
 
 	/**
@@ -167,5 +174,174 @@ class Setup {
 			$fields = include( LSX_TO_PATH . 'includes/metaboxes/config-' . $type . '.php' );
 		}
 		return $fields;
+	}
+
+	/**
+	 * Allow extra tags and attributes to wp_kses_post()
+	 */
+	public function wp_kses_allowed_html( $allowedtags, $context ) {
+		if ( ! isset( $allowedtags['i'] ) ) {
+			$allowedtags['i'] = array();
+		}
+		$allowedtags['i']['aria-hidden']    = true;
+
+		if ( ! isset( $allowedtags['span'] ) ) {
+			$allowedtags['span'] = array();
+		}
+
+		$allowedtags['span']['aria-hidden'] = true;
+
+		if ( ! isset( $allowedtags['button'] ) ) {
+			$allowedtags['button'] = array();
+		}
+
+		$allowedtags['button']['aria-label']   = true;
+		$allowedtags['button']['data-dismiss'] = true;
+
+		if ( ! isset( $allowedtags['li'] ) ) {
+			$allowedtags['li'] = array();
+		}
+
+		$allowedtags['li']['data-target']   = true;
+		$allowedtags['li']['data-slide-to'] = true;
+
+		if ( ! isset( $allowedtags['a'] ) ) {
+			$allowedtags['a'] = array();
+		}
+
+		$allowedtags['a']['data-toggle']             = true;
+		$allowedtags['a']['data-target']             = true;
+		$allowedtags['a']['data-slide']              = true;
+		$allowedtags['a']['data-collapsed']          = true;
+		$allowedtags['a']['data-envira-caption']     = true;
+		$allowedtags['a']['data-envira-retina']      = true;
+		$allowedtags['a']['data-thumbnail']          = true;
+		$allowedtags['a']['data-mobile-thumbnail']   = true;
+		$allowedtags['a']['data-envirabox-type']     = true;
+		$allowedtags['a']['data-video-width']        = true;
+		$allowedtags['a']['data-video-height']       = true;
+		$allowedtags['a']['data-video-aspect-ratio'] = true;
+
+		if ( ! isset( $allowedtags['h2'] ) ) {
+			$allowedtags['h2'] = array();
+		}
+
+		$allowedtags['h2']['data-target'] = true;
+		$allowedtags['h2']['data-toggle'] = true;
+
+		if ( ! isset( $allowedtags['div'] ) ) {
+			$allowedtags['div'] = array();
+		}
+
+		$allowedtags['div']['aria-labelledby']                      = true;
+		$allowedtags['div']['data-interval']                        = true;
+		$allowedtags['div']['data-icon']                            = true;
+		$allowedtags['div']['data-id']                              = true;
+		$allowedtags['div']['data-class']                           = true;
+		$allowedtags['div']['data-long']                            = true;
+		$allowedtags['div']['data-lat']                             = true;
+		$allowedtags['div']['data-zoom']                            = true;
+		$allowedtags['div']['data-link']                            = true;
+		$allowedtags['div']['data-thumbnail']                       = true;
+		$allowedtags['div']['data-title']                           = true;
+		$allowedtags['div']['data-type']                            = true;
+		$allowedtags['div']['data-cluster-small']                   = true;
+		$allowedtags['div']['data-cluster-medium']                  = true;
+		$allowedtags['div']['data-cluster-large']                   = true;
+		$allowedtags['div']['data-fusion-tables']                   = true;
+		$allowedtags['div']['data-fusion-tables-colour-border']     = true;
+		$allowedtags['div']['data-fusion-tables-width-border']      = true;
+		$allowedtags['div']['data-fusion-tables-colour-background'] = true;
+		$allowedtags['div']['itemscope']                            = true;
+		$allowedtags['div']['itemtype']                             = true;
+		$allowedtags['div']['data-row-height']                      = true;
+		$allowedtags['div']['data-justified-margins']               = true;
+		$allowedtags['div']['data-slick']                           = true;
+
+		//Envirta Gallery tags
+		//
+		$allowedtags['div']['data-envira-id']                       = true;
+		$allowedtags['div']['data-gallery-config']                  = true;
+		$allowedtags['div']['data-gallery-images']                  = true;
+		$allowedtags['div']['data-gallery-theme']                   = true;
+		$allowedtags['div']['data-envira-columns']                  = true;
+
+		if ( ! isset( $allowedtags['img'] ) ) {
+			$allowedtags['img'] = array();
+		}
+
+		$allowedtags['img']['data-envira-index']      = true;
+		$allowedtags['img']['data-envira-caption']    = true;
+		$allowedtags['img']['data-envira-gallery-id'] = true;
+		$allowedtags['img']['data-envira-item-id']    = true;
+		$allowedtags['img']['data-envira-src']        = true;
+		$allowedtags['img']['data-envira-srcset']     = true;
+
+		if ( ! isset( $allowedtags['input'] ) ) {
+			$allowedtags['input'] = array();
+		}
+
+		$allowedtags['input']['type']    = true;
+		$allowedtags['input']['id']      = true;
+		$allowedtags['input']['name']    = true;
+		$allowedtags['input']['value']   = true;
+		$allowedtags['input']['size']    = true;
+		$allowedtags['input']['checked'] = true;
+		$allowedtags['input']['onclick'] = true;
+		$allowedtags['input']['class'] = true;
+		$allowedtags['input']['placeholder'] = true;
+		$allowedtags['input']['autocomplete'] = true;
+
+		if ( ! isset( $allowedtags['select'] ) ) {
+			$allowedtags['select'] = array();
+		}
+
+		$allowedtags['select']['name']     = true;
+		$allowedtags['select']['id']       = true;
+		$allowedtags['select']['disabled'] = true;
+		$allowedtags['select']['onchange'] = true;
+
+		if ( ! isset( $allowedtags['option'] ) ) {
+			$allowedtags['option'] = array();
+		}
+
+		$allowedtags['option']['value']    = true;
+		$allowedtags['option']['selected'] = true;
+
+		if ( ! isset( $allowedtags['iframe'] ) ) {
+			$allowedtags['iframe'] = array();
+		}
+
+		$allowedtags['iframe']['src']             = true;
+		$allowedtags['iframe']['width']           = true;
+		$allowedtags['iframe']['height']          = true;
+		$allowedtags['iframe']['frameborder']     = true;
+		$allowedtags['iframe']['allowfullscreen'] = true;
+		$allowedtags['iframe']['style']           = true;
+
+		if ( ! isset( $allowedtags['noscript'] ) ) {
+			$allowedtags['noscript'] = array();
+		}
+
+		return $allowedtags;
+	}
+
+	/**
+	 * Allow extra protocols to wp_kses_post()
+	 */
+	public function kses_allowed_protocols( $allowedprotocols ) {
+		$allowedprotocols[] = 'tel';
+
+		return $allowedprotocols;
+	}
+
+	/**
+	 * Allow extra style attributes to wp_kses_post()
+	 */
+	public function safe_style_css( $allowedstyles ) {
+		$allowedstyles[] = 'display';
+		$allowedstyles[] = 'background-image';
+
+		return $allowedstyles;
 	}
 }
--- includes/classes/legacy/class-frontend.php
+++ includes/classes/legacy/class-frontend.php
@@ -104,27 +104,6 @@ class Frontend extends Tour_Operator {
 		) {
 			$this->enable_modals = true;
 		}
-
-		if ( ( is_post_type_archive( $this->active_post_types ) ) || ( is_tax( array_keys( $this->taxonomies ) ) ) ) {
-			add_filter( 'use_default_gallery_style', '__return_false' );
-
-			add_action( 'lsx_content_wrap_before', 'lsx_to_archive_description', 100 );
-			add_filter( 'lsx_to_archive_description', array( $this, 'get_post_type_archive_description' ), 1, 3 );
-
-			add_action( 'lsx_content_top', array( $this, 'archive_taxonomy_content_part' ), 100 );
-
-			// LSX default pagination
-			add_action( 'lsx_content_bottom', array( 'lsx\legacy\Frontend', 'lsx_default_pagination' ) );
-		}
-	}
-
-	/**
-	 * Taxonomy Archive content part.
-	 */
-	public function archive_taxonomy_content_part() {
-		if ( is_tax( array_keys( $this->taxonomies ) ) && have_posts() ) {
-			lsx_to_content( 'content', get_queried_object()->taxonomy );
-		}
 	}
 
 	/**
@@ -328,13 +307,6 @@ class Frontend extends Tour_Operator {
 		}
 
 		return $title;
-	}
-
-	/**
-	 * Outputs LSX default pagination.
-	 */
-	public static function lsx_default_pagination() {
-		lsx_paging_nav();
 	}
 
 	/**
