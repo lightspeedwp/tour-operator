c03a90ff3df5d96699b12738a75c71ba33196cd9
Warwick <krugazul@gmail.com>
2025-04-29 15:40:30 +0200

    Adding in the Disable archives and single posts.

--- vendor/content-models/includes/runtime/class-content-model.php
+++ vendor/content-models/includes/runtime/class-content-model.php
@@ -191,13 +191,7 @@ final class Content_Model {
 			$post_type_args['supports'][]   = 'page-attributes';
 		}
 
-		$post_type_args = apply_filters( 'content_model_post_type_args', $post_type_args );
-
-		$options = get_option( 'lsx_to_settings', [] );
-		print_r('<pre>');
-		print_r($options);
-		print_r('</pre>');
-		die();
+		$post_type_args = apply_filters( 'content_model_post_type_args', $post_type_args, $this->slug );
  
 		register_post_type(
 			$this->slug,
