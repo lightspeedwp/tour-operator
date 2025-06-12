7921e44624460cb5cb4801192cce8aa5d37783ac
Warwick <krugazul@gmail.com>
2025-06-12 15:20:21 +0200

    Adding in the pattern selector for the modals #362

--- includes/classes/frontend/class-modals.php
+++ includes/classes/frontend/class-modals.php
@@ -39,7 +39,7 @@ class Modals {
 		$this->enable_modals = true;
 		add_filter( 'lsx_to_connected_list_item', array( $this, 'add_modal_attributes' ), 10, 3 );
 
-		add_action( 'wp_footer', array( $this, 'output_modals' ), 10 );
+		add_action( 'wp_footer', array( $this, 'output_modals2' ), 10 );
 	}
 
 	/**
@@ -65,13 +65,31 @@ class Modals {
 			return $content;
 		}
 
-		ob_start();
 		foreach ( $this->modal_ids as $post_id ) {
 			?>
 			<dialog id="to-modal-<?php echo esc_attr( $post_id ); ?>" class="wp-block-hm-popup" data-trigger="click" data-expiry="7" data-backdrop-opacity="0.75">
-				<div class="wp-block-group is-layout-flow wp-block-group-is-layout-flow">
-				<p>Title</p>
-				</div>
+				<?php
+					$post_type = get_post_type( $post_id );
+					switch ( $post_type ) {
+						case 'accommodation':
+							$template = '<!-- wp:pattern {"slug":"lsx-tour-operator/accommodation-card"} /-->';	
+						break;
+						
+						case 'destination':
+							$template = '<!-- wp:pattern {"slug":"lsx-tour-operator/destination-card"} /-->';	
+						break;
+
+						case 'tour':
+							$template = '<!-- wp:pattern {"slug":"lsx-tour-operator/tour-card"} /-->';	
+						break;
+
+						default:
+							$template = '<p>' . __( 'Please select a pattern or customize your layout with the Tour Operator blocks.', 'tour-operator' ) . '</p>';
+						break;
+					}
+					
+					echo do_blocks( $template );
+				?>
 			</dialog>
 			<?php
 		}
@@ -79,5 +97,72 @@ class Modals {
 		$temp = ob_get_clean();
 
 		 echo ( $temp );
+	}
+
+	/**
+	 * a filter to overwrite the links with modal tags.
+	 */
+	public function output_modals2( $content = '' ) {
+		if ( false === $this->enable_modals || empty( $this->modal_ids ) ) {
+			return;
+		}
+
+		do_action( 'qm/debug', $this->modal_ids );
+
+		$modal_args  = [
+			'post__in' => $this->modal_ids,
+			'post_status' => 'publish',
+			'post_type' => 'any',
+			'ignore_sticky_posts' => true,
+			'posts_per_page' => -1,
+			'nopagin' => true,
+		];
+		$modal_query = new \WP_Query( $modal_args );
+		$modal_html  = [];
+
+		do_action( 'qm/debug', $modal_query );
+
+		if ( $modal_query->have_posts() ) {
+			while ( $modal_query->have_posts() ) {
+				$modal_query->the_post();
+
+				do_action( 'qm/debug', $modal_query->post );
+
+				$modal_id  = get_the_ID();
+				$temp_html = '<dialog id="to-modal-' . $modal_id . '" class="wp-block-hm-popup" data-trigger="click" data-expiry="7" data-backdrop-opacity="0.75">';
+
+				$post_type = get_post_type();
+				switch ( $post_type ) {
+					case 'accommodation':
+						$template = '<!-- wp:pattern {"slug":"lsx-tour-operator/accommodation-card"} /-->';	
+					break;
+					
+					case 'destination':
+						$template = '<!-- wp:pattern {"slug":"lsx-tour-operator/destination-card"} /-->';	
+					break;
+
+					case 'tour':
+						$template = '<!-- wp:pattern {"slug":"lsx-tour-operator/tour-card"} /-->';	
+					break;
+
+					default:
+						$template = '<p>' . __( 'Please select a pattern or customize your layout with the Tour Operator blocks.', 'tour-operator' ) . '</p>';
+					break;
+				}
+
+				$temp_html .= do_blocks( $template );
+				$temp_html .= '</dialog>';
+
+				$modal_html[] = $temp_html;
+			}
+
+			wp_reset_postdata();
+		}
+
+		if ( ! empty( $modal_html ) ) {
+			$content .= implode( '', $modal_html );
+		}
+
+		echo $content;
 	}
 }
