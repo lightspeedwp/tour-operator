c92e40431c685ee9f198ca9f80a550261cd63fb2
tibibuzdu <buzdugan.tiberiu@gmail.com>
2025-11-16 18:44:22 +0200

    Update blocks with i18n support, improved keywords and examples
    - Enhanced units block with conditional registration and improved content.
    - Renamed lsx-destination-to-tour block to destination-to-tour.

--- build/blocks/destination-to-accommodation/index.asset.php
+++ build/blocks/destination-to-accommodation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'a03c13c6092847143526');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => 'd77ee57480a2a908d0a8');
---
+++ build/blocks/destination-to-tour/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-i18n'), 'version' => 'babb924e6e2db3ecbe0a');
