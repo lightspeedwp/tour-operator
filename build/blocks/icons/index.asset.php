8ea9d0e779b0bb4e32d92a0a540ef68d779ed8b3
tibibuzdu <buzdugan.tiberiu@gmail.com>
2025-12-04 12:30:03 +0200

    Implement server-side rendering for Icons block for cleaner templates

--- build/blocks/icons/index.asset.php
+++ build/blocks/icons/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f356a4d23143ef1eea07');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f1fc58332b32676cbe6d');
