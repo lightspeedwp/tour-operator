4d955fee373b20c69f7d85ffdbf5830574f36f0e
Warwick <krugazul@gmail.com>
2025-12-10 21:51:33 +0200

    Update sticky menu block and dependencies
    - Bump version in index.asset.php for sticky menu block.
    - Refactor index.js for sticky menu block to improve readability and maintainability.
    - Remove unnecessary "peer" property from package-lock.json.
    - Update accommodation-type block to remove wp.escapeHtml for prefix.

--- build/blocks/booking-validity/index.asset.php
+++ build/blocks/booking-validity/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => 'd986832a66261c3e5b2f');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '4bf4399820137642a46a');
