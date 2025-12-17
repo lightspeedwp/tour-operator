6f4c2751c2361d9e098d21cd738aac69db7432e5
Warwick <krugazul@gmail.com>
2025-12-17 14:12:11 +0200

    Update asset versions and enhance destination-to-tour block with parent argument for post connection

--- build/blocks/destination-to-tour/index.asset.php
+++ build/blocks/destination-to-tour/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => '8d09f715be4f181bbfe6');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => 'aac926357ff1bfa1b40c');
