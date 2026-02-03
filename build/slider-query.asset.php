fc7b979d51b463e442732e1394e310e5879738a1
tibibuzdu <buzdugan.tiberiu@gmail.com>
2026-02-03 11:17:41 +0200

    Clean up on-sale class when switching post type

--- build/slider-query.asset.php
+++ build/slider-query.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'fd55f77a5d9aaa613dcc');
+<?php return array('dependencies' => array(), 'version' => 'ad68088f3d26b9a98408');
--- src/js/blocks/slider-query.js
+++ src/js/blocks/slider-query.js
@@ -11,6 +11,29 @@
                     return el( BlockEdit, props );
                 }
 
+                // Clean up on-sale class and attribute when post type changes from tour to something else
+                const currentPostType = props.attributes.query && props.attributes.query.postType;
+                const previousPostType = wp.element.useRef( currentPostType );
+                
+                wp.element.useEffect( () => {
+                    if ( previousPostType.current === 'tour' && currentPostType !== 'tour' ) {
+                        // Post type changed from tour to something else, clear on-sale
+                        const hasOnSale = props.attributes.filterByOnsale || 
+                            ( props.attributes.className && props.attributes.className.includes( 'on-sale' ) );
+                        
+                        if ( hasOnSale ) {
+                            const newAttributes = { filterByOnsale: false };
+                            let className = props.attributes.className || '';
+                            className = className.replace( /\bon-sale\b\s*/g, '' ).trim();
+                            if ( className ) {
+                                newAttributes.className = className;
+                            }
+                            props.setAttributes( newAttributes );
+                        }
+                    }
+                    previousPostType.current = currentPostType;
+                }, [ currentPostType ] );
+
                 let hasCustomClass = props.attributes.hasCustomClass || false;
                 if ( undefined === props.attributes.hasCustomClass ) {
                     if (
