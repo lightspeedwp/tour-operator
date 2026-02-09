21aeaf19b31cdd7b6657608589d83a3a05baff0d
Warwick <krugazul@gmail.com>
2026-02-09 11:04:49 +0200

    Fix: Extend location condition to include 'wetu_map' and 'google_map' in query loop

--- includes/classes/blocks/class-query-loop.php
+++ includes/classes/blocks/class-query-loop.php
@@ -183,7 +183,7 @@ class Query_Loop {
 			if ( empty( wp_get_post_terms( get_the_ID(), $key, $tax_args ) ) ) {
 				$block_content = '';
 			}
-		} elseif ( 'location' === $key ) {
+		} elseif ( 'location' === $key || 'wetu_map' === $key || 'google_map' === $key ) {
 			if ( ! lsx_to_has_map() ) {
 				$block_content = '';
 			}
